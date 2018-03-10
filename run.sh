d73cfd2285a1875a4a1960c8ffc55f67d2b99aff
Gido Kuechler <gido@ukuechler.de>
2018-03-10 16:56:35 +0100

    mostly same compose file per platform

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-docker-compose up -d
+if [ "$(uname -m)" = "x86_64" ]; then 
+    docker-compose up -d
+else
+    docker-compose -f docker-compose-raspi.yml up -d
+fi
+
 
 echo "Grafana: http://127.0.0.1:3000 - admin/admin"
-
 echo
 echo "Current database list"
-curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
+curl -G "http://localhost:8086/query?u=admin;p=admin;pretty=true" --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
 
 echo
 echo "Create a new database ?"
-echo "curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE mydb'"
+echo "curl -XPOST 'http://localhost:8086/query?u=admin;p=admin' --data-urlencode 'q=CREATE DATABASE mydb'"
+
+docker ps -a
+
